af0ca9114c987abd2470eed7767c0fa841378a62
Mohammed_Raiful_Alam <97615466+Raifulalam@users.noreply.github.com>
2025-10-29 11:03:09 +0530

    Time: 0 ms (100%), Space: 40.7 MB (72.64%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int smallestNumber(int n) {
+        return(1<<bitLength(n))-1;
+    }
+    private int bitLength(int n){
+        return Integer.SIZE-Integer.numberOfLeadingZeros(n);
+    }
+}
